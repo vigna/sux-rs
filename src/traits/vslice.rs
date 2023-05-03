9e57fef2903f00999bd105878ae345e4a7899ac7
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-05-03 10:47:07 +0200

    Added docs and set implementation

--- src/traits/vslice.rs
+++ src/traits/vslice.rs
@@ -5,8 +5,19 @@
 //! Implementing the slice trait would be more natural, but it would be very complicated
 //! because there is no easy way to return a reference to a bit segment 
 //! (see, e.g., [BitSlice](https://docs.rs/bitvec/latest/bitvec/slice/struct.BitSlice.html)).
+//! 
+//! Each `VSlice` has an associated [`BIT_WIDTH`]. All stored values must fit
+//! within this bit width.
+//! 
+//! Implementations must return always zero on a [`VSlice::get`] when the bit 
+//! width is zero. The behavior of a [`VSlice::set`] in the same context is not defined.
+
+use anyhow::Result;
 
 pub trait VSlice {
+    /// Return the width of the slice. All elements stored in the slice must
+    /// fit within this bit width.
+    fn bit_width(&self) -> usize;
     /// Return the length of the slice.
     fn len(&self) -> usize;
     /// Return the element of the slice at the given position, without
@@ -24,7 +35,23 @@ pub trait VSlice {
     }
 }
 
+
 pub trait VSliceMut: VSlice {
+    /// Set the element of the slice at the given position, without
+    /// doing any bound or value checking.
     unsafe fn set_unchecked(&self, index: usize, value: u64);
-    fn set(&self, index: usize, value: u64) -> Option<()>;
+    /// Set the element of the slice at the given position, or return `None` if the
+    /// position is out of bounds or the value does not fit in [`VSlice::bit_width`] bits.
+    fn set(&self, index: usize, value: u64) -> Result<u64> {
+        if index >= self.len() {
+            Err(anyhow::anyhow!("Index out of bounds"))
+        } else if value & (u64::MAX >> 64 - self.bit_width()) != value {
+            Err(anyhow::anyhow!("Value does not fit in {} bits", self.bit_width()))
+        } else {
+            unsafe {
+                self.set_unchecked(index, value);
+            }
+            Ok(value)
+        }
+    }
 }
