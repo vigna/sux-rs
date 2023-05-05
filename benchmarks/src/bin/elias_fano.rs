4a036a151950bc9e87b83f5efb4bedb9815036de
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-05-05 09:59:21 +0200

    Benchmarks

---
+++ benchmarks/src/bin/elias_fano.rs
@@ -0,0 +1,66 @@
+use sux::prelude::*;
+use std::hint::black_box;
+use rand::rngs::SmallRng;
+use rand::Rng;
+use rand::SeedableRng;
+use clap::Parser;
+
+use std::time::Instant;
+
+#[derive(Parser, Debug)]
+#[command(about = "Benchmarks the Rust Sux implementation", long_about = None)]
+struct Args {
+    /// The number of elements
+    n: u64,
+
+    /// The size of the universe
+    u: u64,
+
+    /// The number of values to test
+    t: usize,
+
+    /// The number of test repetitions
+    #[arg(short, long, default_value = "0.5")]
+    density: f64,
+
+    /// The number of test repetitions
+    #[arg(short, long, default_value = "10")]
+    repeats: usize,
+}
+
+fn main() {
+    let args = Args::parse();
+    let mut values = Vec::with_capacity(args.n as usize);
+    let mut rng = SmallRng::seed_from_u64(0);
+    for _ in 0..args.n {
+        values.push(rng.gen_range(0..args.u));
+    }
+    values.sort();
+    let mut elias_fano_builder = EliasFanoBuilder::new(args.u, args.n);
+    for value in values {
+        elias_fano_builder.push(value).unwrap();
+    }
+    let elias_fano: EliasFano<
+        SparseIndex<BitMap<Vec<u64>>, Vec<u64>, 8>, 
+        CompactArray<Vec<u64>>,
+    > = elias_fano_builder.build().convert_to().unwrap();
+
+    let mut ranks = Vec::with_capacity(args.t);
+    for _ in 0..args.t {
+        ranks.push(rng.gen_range(0..args.n as usize));
+    }
+
+    let mut u: usize = 0;
+
+    for _ in 0..args.repeats {
+        let start = Instant::now();
+        for &rank in &ranks {
+            unsafe{u += elias_fano.select_unchecked(rank);}
+        }
+        let duration = start.elapsed();
+
+        println!("EliasFano select {}ns", duration.as_secs_f64() * 1.0e9 / args.t as f64);  
+    }
+
+    black_box(u);
+}
