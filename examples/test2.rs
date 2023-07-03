9b1b7a01213fbfdaa0c2058ce9fff8383f8245e7
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-07-03 05:20:19 +0200

    Test2

---
+++ examples/test2.rs
@@ -0,0 +1,33 @@
+use rand::rngs::SmallRng;
+use rand::rngs::StdRng;
+use rand::Rng;
+use rand::SeedableRng;
+use rand_chacha::ChaCha8Rng;
+use std::hint::black_box;
+use std::time::Instant;
+
+fn main() {
+    const N: usize = 1024 * 1024 * 1024;
+    let mut data = Vec::<u64>::new();
+    data.resize(N, 0);
+    let fill = data.as_mut_slice();
+
+    let mut r = StdRng::seed_from_u64(0);
+    let start = Instant::now();
+    black_box(r.fill(fill));
+    println!("ChaCha12 {}", start.elapsed().as_nanos() as f64 / N as f64);
+
+    let mut r = ChaCha8Rng::seed_from_u64(0);
+    let start = Instant::now();
+    black_box(r.fill(fill));
+    println!("ChaCha8 {}", start.elapsed().as_nanos() as f64 / N as f64);
+
+    let mut r = SmallRng::seed_from_u64(0);
+    let start = Instant::now();
+    black_box(r.fill(fill));
+
+    println!(
+        "xoshiro256++ {}",
+        start.elapsed().as_nanos() as f64 / N as f64
+    );
+}
