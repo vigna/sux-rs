e7eb6d98935ff3b99d49c3f4fe8c50408d046e27
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-09-07 09:57:20 +0200

    Tests for bitmaps

--- tests/bitmap.rs
+++ tests/bitmap.rs
@@ -1,7 +1,8 @@
 use core::sync::atomic::{AtomicU64, Ordering};
+use epserde::*;
 use rand::rngs::SmallRng;
 use rand::seq::SliceRandom;
-use rand::SeedableRng;
+use rand::{RngCore, SeedableRng};
 use sux::prelude::*;
 
 #[test]
@@ -96,5 +97,42 @@ fn test_bitmap() {
         for i in 0..u {
             assert_eq!(bm.get_atomic(i, Ordering::Relaxed), 0);
         }
+    }
+}
+
+#[test]
+fn test_epsserde() {
+    let mut rng = SmallRng::seed_from_u64(0);
+    let mut b = BitMap::new(200);
+    for i in 0..200 {
+        b.set(i, rng.next_u64() % 2);
+    }
+
+    let tmp_file = std::env::temp_dir().join("test_serdes_ef.bin");
+    let mut file = std::io::BufWriter::new(std::fs::File::create(&tmp_file).unwrap());
+    b.serialize(&mut file).unwrap();
+    drop(file);
+
+    let c = epserde::map::<BitMap<Vec<u64>>>(&tmp_file, epserde::Flags::empty()).unwrap();
+
+    for i in 0..200 {
+        assert_eq!(b.get(i), c.get(i));
+    }
+
+    let mut b = CountingBitmap::new(200);
+    for i in 0..200 {
+        b.set(i, rng.next_u64() % 2);
+    }
+
+    let tmp_file = std::env::temp_dir().join("test_serdes_ef.bin");
+    let mut file = std::io::BufWriter::new(std::fs::File::create(&tmp_file).unwrap());
+    b.serialize(&mut file).unwrap();
+    drop(file);
+
+    let c = epserde::map::<CountingBitmap<Vec<u64>, usize>>(&tmp_file, epserde::Flags::empty())
+        .unwrap();
+
+    for i in 0..200 {
+        assert_eq!(b.get(i), c.get(i));
     }
 }
