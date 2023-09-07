ca2e30105c2dae7d2a86b220a029e746192b16c0
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2023-09-07 11:42:06 +0200

    fixed convert_to for bitmap

--- tests/elias_fano.rs
+++ tests/elias_fano.rs
@@ -2,7 +2,32 @@ use anyhow::Result;
 use rand::rngs::SmallRng;
 use rand::Rng;
 use rand::SeedableRng;
+use std::sync::atomic::Ordering;
 use sux::prelude::*;
+
+#[test]
+#[cfg(feature = "rayon")]
+fn test_elias_fano_concurrent() -> Result<()> {
+    use rayon::prelude::*;
+    let mut rng = SmallRng::seed_from_u64(0);
+    for (u, n) in [(1000, 100), (100, 100), (100, 1000)] {
+        let mut values = (0..n).map(|_| rng.gen_range(0..u)).collect::<Vec<_>>();
+
+        values.sort();
+
+        // create the builder for the "in memory" elias-fano
+        let efb = EliasFanoAtomicBuilder::new(u, n);
+        // push concurrently the values
+        values
+            .par_iter()
+            .enumerate()
+            .for_each(|(index, value)| unsafe { efb.set(index, *value, Ordering::SeqCst) });
+        // Finish the creation of elias-fano
+        let ef = efb.build();
+        println!("{:?}", ef);
+    }
+    Ok(())
+}
 
 #[test]
 fn test_elias_fano() -> Result<()> {
