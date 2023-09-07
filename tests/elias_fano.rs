b9923d303f6c491e5ec5bc15808fa4b2c72ec8e6
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2023-09-07 12:05:24 +0200

    epserde works with elias-fano!

--- tests/elias_fano.rs
+++ tests/elias_fano.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
+use epserde::*;
 use rand::rngs::SmallRng;
 use rand::Rng;
+use rand::RngCore;
 use rand::SeedableRng;
 use std::sync::atomic::Ordering;
 use sux::prelude::*;
@@ -76,5 +78,47 @@ fn test_elias_fano() -> Result<()> {
         println!("{:?}", ef);
     }
 
+    Ok(())
+}
+
+#[test]
+fn test_epsserde() -> Result<()> {
+    let mut rng = SmallRng::seed_from_u64(0);
+    for (u, n) in [(1000, 100), (100, 100), (100, 1000)] {
+        let mut values = (0..n).map(|_| rng.gen_range(0..u)).collect::<Vec<_>>();
+
+        values.sort();
+
+        // create the builder for the "in memory" elias-fano
+        let mut efb = EliasFanoBuilder::new(u, n);
+        // push the values
+        for value in values.iter() {
+            efb.push(*value)?;
+        }
+        // Finish the creation of elias-fano
+        let ef: DefaultEliasFano = efb.build();
+        // Add the ones indices
+        let ef: EliasFano<
+            SparseIndex<CountingBitmap<Vec<u64>, usize>, Vec<u64>, 8>,
+            CompactArray<Vec<u64>>,
+        > = ef.convert_to().unwrap();
+
+        let tmp_file = std::env::temp_dir().join("test_serdes_ef.bin");
+        let mut file = std::io::BufWriter::new(std::fs::File::create(&tmp_file)?);
+        let schema = ef.serialize_with_schema(&mut file)?;
+        drop(file);
+        println!("{}", schema.to_csv());
+
+        let c = epserde::map::<
+            EliasFano<
+                SparseIndex<CountingBitmap<Vec<u64>, usize>, Vec<u64>, 8>,
+                CompactArray<Vec<u64>>,
+            >,
+        >(&tmp_file, epserde::Flags::empty())?;
+
+        for i in 0..n {
+            assert_eq!(ef.get(i as usize), c.get(i as usize));
+        }
+    }
     Ok(())
 }
