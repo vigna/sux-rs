88ee0a666ef3056237bc61984b851587e1094e9f
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-10-27 12:25:55 +0200

    In-memory build working

--- src/func/vfunc.rs
+++ src/func/vfunc.rs
@@ -5,12 +5,17 @@
 * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
 */
 
+<<<<<<< Updated upstream
 // We import selectively to be able to use AtomicHelper
 use crate::bits::*;
 use crate::traits::bit_field_slice;
 use crate::utils::*;
 use arbitrary_chunks::ArbitraryChunks;
 use bit_field_slice::{BitFieldSliceCore, Word};
+=======
+use crate::prelude::bit_field_slice::Word;
+use crate::prelude::*;
+>>>>>>> Stashed changes
 use common_traits::{AsBytes, AtomicUnsignedInt, IntoAtomic};
 use dsi_progress_logger::ProgressLogger;
 use epserde::prelude::*;
@@ -161,6 +166,7 @@ fn compute_params(num_keys: usize) -> (u32, usize, usize, f64) {
     (chunk_high_bits, max_num_threads, log2_l, c)
 }
 
+<<<<<<< Updated upstream
 enum ParSolveResult<O: Word + IntoAtomic> {
     DuplicateSignature,
     CantPeel,
@@ -171,6 +177,12 @@ fn par_solve<
     'a,
     O: Word + ZeroCopy + Send + Sync + IntoAtomic + 'a,
     I: Iterator<Item = (usize, &'a [([u64; 2], O)])> + Send + Sync,
+=======
+fn solve<
+    'a,
+    O: Word + ZeroCopy + Send + Sync + IntoAtomic + 'a,
+    I: Iterator<Item = (usize, &'a [([u64; 2], O)])>,
+>>>>>>> Stashed changes
 >(
     chunk_iter: I,
     bit_width: usize,
@@ -179,11 +191,16 @@ fn par_solve<
     num_threads: usize,
     segment_size: usize,
     log2_l: u32,
+<<<<<<< Updated upstream
 ) -> ParSolveResult<O>
 where
     O::AtomicType: AtomicUnsignedInt + AsBytes,
 {
     use crate::traits::bit_field_slice::AtomicHelper;
+=======
+) -> Option<AtomicBitFieldVec<O>> {
+    use crate::traits::bit_field_slice::AtomicBitFieldSlice;
+>>>>>>> Stashed changes
     let data = AtomicBitFieldVec::<O>::new(bit_width, num_vertices * num_chunks);
     let mutex = std::sync::Arc::new(Mutex::new(chunk_iter));
     let fail = AtomicBool::new(false);
@@ -264,18 +281,18 @@ where
                         *v += chunk_offset;
                     });
                     let value = if v == edge[0] {
-                        data.get(edge[1], Relaxed) ^ data.get(edge[2], Relaxed)
+                        data.get_atomic(edge[1], Relaxed) ^ data.get_atomic(edge[2], Relaxed)
                     } else if v == edge[1] {
-                        data.get(edge[0], Relaxed) ^ data.get(edge[2], Relaxed)
+                        data.get_atomic(edge[0], Relaxed) ^ data.get_atomic(edge[2], Relaxed)
                     } else {
-                        data.get(edge[0], Relaxed) ^ data.get(edge[1], Relaxed)
+                        data.get_atomic(edge[0], Relaxed) ^ data.get_atomic(edge[1], Relaxed)
                     };
                     // TODO
-                    data.set(v, sigs[edge_index].1 ^ value, Relaxed);
+                    data.set_atomic(v, sigs[edge_index].1 ^ value, Relaxed);
                     debug_assert_eq!(
-                        data.get(edge[0], Relaxed)
-                            ^ data.get(edge[1], Relaxed)
-                            ^ data.get(edge[2], Relaxed),
+                        data.get_atomic(edge[0], Relaxed)
+                            ^ data.get_atomic(edge[1], Relaxed)
+                            ^ data.get_atomic(edge[2], Relaxed),
                         sigs[edge_index].1
                     );
                 }
@@ -525,6 +542,196 @@ where
                 bit_width = max_value.len() as usize;
                 info!("max value = {}, bit width = {}", max_value, bit_width);
 
+<<<<<<< Updated upstream
+=======
+            if let Some(pl) = pl.as_mut() {
+                pl.done_with_count(num_keys);
+            }
+
+            let mut cumul = vec![0; num_chunks + 1];
+            for i in 0..num_chunks {
+                cumul[i + 1] = cumul[i] + chunk_sizes[i];
+            }
+
+            let segment_size =
+                ((*chunk_sizes.iter().max().unwrap() as f64 * c).ceil() as usize + l + 1) / (l + 2);
+            let num_vertices = segment_size * (l + 2);
+            info!(
+                "Size {:.2}%",
+                (100.0 * (num_vertices * num_chunks) as f64) / (sigs.len() as f64 * c)
+            );
+
+            let data = AtomicBitFieldVec::<O>::new(bit_width, num_vertices * num_chunks);
+
+            let chunk = AtomicUsize::new(0);
+            let fail = AtomicBool::new(false);
+            let num_threads = num_chunks.min(num_cpus::get()).min(max_num_threads);
+            info!("Using {} threads", num_threads);
+
+            thread::scope(|s| {
+                for _ in 0..num_threads {
+                    s.spawn(|| loop {
+                        let chunk = chunk.fetch_add(1, Relaxed);
+                        if chunk >= num_chunks {
+                            break;
+                        }
+                        let sigs = &sigs[cumul[chunk]..cumul[chunk + 1]];
+
+                        let mut pl = ProgressLogger::default();
+                        pl.expected_updates = Some(sigs.len());
+
+                        pl.start(format!(
+                            "Generating graph for chunk {}/{}...",
+                            chunk, num_chunks
+                        ));
+                        let mut edge_lists = Vec::new();
+                        edge_lists.resize_with(num_vertices, EdgeList::default);
+
+                        sigs.iter().enumerate().for_each(|(edge_index, sig)| {
+                            for &v in Self::edge(&sig.0, log2_l, segment_size).iter() {
+                                edge_lists[v].add(edge_index);
+                            }
+                        });
+                        pl.done_with_count(sigs.len());
+
+                        let next = AtomicUsize::new(0);
+                        let incr = 1024;
+
+                        pl.start(format!(
+                            "Peeling graph for chunk {}/{}...",
+                            chunk, num_chunks
+                        ));
+
+                        let mut stack = Vec::new();
+                        loop {
+                            if fail.load(Ordering::Relaxed) {
+                                return;
+                            }
+                            let start = next.fetch_add(incr, Ordering::Relaxed);
+                            if start >= num_vertices {
+                                break;
+                            }
+                            for v in start..(start + incr).min(num_vertices) {
+                                if edge_lists[v].degree() != 1 {
+                                    continue;
+                                }
+                                let mut pos = stack.len();
+                                let mut curr = stack.len();
+                                stack.push(v);
+
+                                while pos < stack.len() {
+                                    let v = stack[pos];
+                                    pos += 1;
+
+                                    if edge_lists[v].degree() == 0 {
+                                        continue; // Skip no longer useful entries
+                                    }
+
+                                    edge_lists[v].dec();
+                                    let edge_index = edge_lists[v].edge_index();
+
+                                    stack[curr] = v;
+                                    curr += 1;
+                                    // Degree is necessarily 0
+                                    for &x in
+                                        Self::edge(&sigs[edge_index].0, log2_l, segment_size).iter()
+                                    {
+                                        if x != v {
+                                            edge_lists[x].remove(edge_index);
+                                            if edge_lists[x].degree() == 1 {
+                                                stack.push(x);
+                                            }
+                                        }
+                                    }
+                                }
+                                stack.truncate(curr);
+                            }
+                        }
+                        if sigs.len() != stack.len() {
+                            fail.store(true, Ordering::Relaxed);
+                        }
+                        pl.done_with_count(sigs.len());
+
+                        pl.start(format!(
+                            "Assigning values for chunk {}/{}...",
+                            chunk, num_chunks
+                        ));
+                        while let Some(mut v) = stack.pop() {
+                            let edge_index = edge_lists[v].edge_index();
+                            let mut edge = Self::edge(&sigs[edge_index].0, log2_l, segment_size);
+                            let chunk_offset = chunk * num_vertices;
+                            v += chunk_offset;
+                            edge.iter_mut().for_each(|v| {
+                                *v += chunk_offset;
+                            });
+                            let value = if v == edge[0] {
+                                data.get(edge[1], Relaxed) ^ data.get(edge[2], Relaxed)
+                            } else if v == edge[1] {
+                                data.get(edge[0], Relaxed) ^ data.get(edge[2], Relaxed)
+                            } else {
+                                data.get(edge[0], Relaxed) ^ data.get(edge[1], Relaxed)
+                            };
+                            // TODO
+                            data.set(v, sigs[edge_index].1 ^ value, Relaxed);
+
+                            debug_assert_eq!(
+                                data.get(edge[0], Relaxed)
+                                    ^ data.get(edge[1], Relaxed)
+                                    ^ data.get(edge[2], Relaxed),
+                                sigs[edge_index].1
+                            );
+                        }
+                        pl.done_with_count(sigs.len());
+                        pl.start(format!("Completed chunk {}/{}.", chunk, num_chunks));
+                    });
+                }
+            });
+
+            if fail.load(Ordering::Relaxed) {
+                warn!("Failed peeling, trying again...")
+            } else {
+                info!(
+                    "bits/keys: {}",
+                    data.len() as f64 * bit_width as f64 / sigs.len() as f64,
+                );
+                return Ok(VFunc {
+                    seed,
+                    log2_l,
+                    high_bits: chunk_high_bits,
+                    chunk_mask,
+                    num_keys: sigs.len(),
+                    segment_size,
+                    values: data.into(),
+                    _marker_t: std::marker::PhantomData,
+                    _marker_o: std::marker::PhantomData,
+                });
+            }
+        }
+        unreachable!("There are infinite possible seeds.")
+    }
+
+    pub fn new_offline<
+        I: std::iter::IntoIterator<Item = T> + Clone,
+        V: std::iter::IntoIterator<Item = O> + Clone,
+    >(
+        keys: I,
+        into_values: &V,
+        pl: &mut Option<&mut ProgressLogger>,
+    ) -> anyhow::Result<VFunc<T, O>> {
+        use crate::traits::bit_field_slice::AtomicBitFieldSlice;
+        // Loop until success or duplicate detection
+        let mut dup_count = 0;
+        for seed in 0.. {
+            if let Some(pl) = pl.as_mut() {
+                pl.start("Reading input...")
+            }
+
+            let store_bits = 12;
+            let mut sig_sorter = SigStore::<O>::new(8, store_bits).unwrap();
+            let mut values = into_values.clone().into_iter();
+            let mut max_value = O::ZERO;
+            sig_sorter.extend(keys.clone().into_iter().map(|x| {
+>>>>>>> Stashed changes
                 if let Some(pl) = pl.as_mut() {
                     pl.done_with_count(num_keys);
                 }
