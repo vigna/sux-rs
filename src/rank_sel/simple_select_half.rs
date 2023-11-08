9e54aeeb30773800d12dc7c87ba38d1357af9024
Tommaso Fontana <tommaso.fontana.ext@wuerth-phoenix.net>
2023-11-08 11:42:39 +0100

    interim fixing simple select

--- src/rank_sel/simple_select_half.rs
+++ src/rank_sel/simple_select_half.rs
@@ -44,19 +44,20 @@ impl<
 }
 
 impl<
-        B: SelectHinted + AsRef<[usize]>,
+        B: SelectHinted + AsRef<[usize]> + BitLength,
         const LOG2_ONES_PER_INVENTORY: usize,
         const LOG2_U64_PER_SUBINVENTORY: usize,
     > SimpleSelectHalf<B, Vec<u64>, LOG2_ONES_PER_INVENTORY, LOG2_U64_PER_SUBINVENTORY>
 {
     pub fn new(bitvec: B) -> Self {
         // estimate the number of ones with our core assumption!
-        let expected_ones = bitvec.len() / 2;
+        let expected_ones = BitLength::len(&bitvec) / 2;
         // number of inventories we will create
         let inventory_size =
             1 + (expected_ones + Self::ONES_PER_INVENTORY - 1) / Self::ONES_PER_INVENTORY;
+        let inventory_len = inventory_size * (Self::U64_PER_SUBINVENTORY + 1);
         // inventory_size, an u64 for the first layer index, and Self::U64_PER_SUBINVENTORY for the sub layer
-        let mut inventory = vec![0; inventory_size];
+        let mut inventory = vec![0; inventory_len];
         // scan the bitvec and fill the first layer of the inventory
         let mut number_of_ones = 0;
         let mut next_quantum = 0;
@@ -67,17 +68,19 @@ impl<
             while number_of_ones + ones_in_word > next_quantum {
                 let in_word_index = word.select_in_word((next_quantum - number_of_ones) as usize);
                 let index = (i * usize::BITS as usize) + in_word_index;
-
+                
                 // write the one in the inventory
                 inventory[ptr] = index as u64;
 
-                ptr += 1;
+                ptr += Self::U64_PER_SUBINVENTORY;
                 next_quantum += Self::ONES_PER_INVENTORY as u64;
             }
             number_of_ones += ones_in_word;
         }
         // in the last inventory write the number of bits
-        inventory[ptr] = bitvec.len() as u64;
+        inventory[ptr] = BitLength::len(&bitvec) as u64;
+        
+        dbg!(&inventory);
 
         // build the index (in parallel if rayon enabled)
         let iter = 0..inventory_size;
@@ -88,6 +91,7 @@ impl<
         iter.for_each(|inventory_idx| {
             let start_idx = inventory_idx * (1 + Self::U64_PER_SUBINVENTORY);
             let end_idx = start_idx + 1 + Self::U64_PER_SUBINVENTORY;
+
             let start_bit_idx = inventory[start_idx];
             let end_bit_idx = inventory[start_idx + 1];
             let end_word_idx = end_bit_idx / usize::BITS as u64;
@@ -102,7 +106,11 @@ impl<
             let mut number_of_ones = inventory_idx * Self::ONES_PER_INVENTORY;
             let mut next_quantum = number_of_ones;
 
+<<<<<<< Updated upstream
             let subinventory = unsafe { inventory[start_idx..end_idx].align_to_mut().1 };
+=======
+            let subinventory = unsafe{inventory[start_idx..end_idx].align_to_mut().1};
+>>>>>>> Stashed changes
 
             let (quantum, size) = if span < u16::MAX as u64 {
                 (Self::ONES_PER_SUB16, core::mem::size_of::<u16>())
