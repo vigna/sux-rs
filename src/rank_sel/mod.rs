526b07442734a97ec0e01d12e4ea11ed421beed6
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-11-26 11:54:43 +0100

    Fixed hinting names, added RankHinted

--- src/rank_sel/mod.rs
+++ src/rank_sel/mod.rs
@@ -9,6 +9,19 @@
 
 Support for rank and select operations.
 
+## Design
+
+Rank and selection structures can be combined arbitrarily with a mix-and-match approach.
+The base class, usually [`BitVec`](crate::bits::bit_vec::BitVec), must implement the hinted
+versions of the desired traits (i.e.,  [`RankHinted`](crate::traits::RankHinted),
+[`SelectHinted`](crate::traits::SelectHinted), and [`SelectZeroHinted`](crate::traits::SelectZeroHinted)).
+At that point, one can add an optional ranking structure, an optional selection structure, and an optional zero-selection
+structure, in this order. The structures implement the ranking or selection traits by providing
+a hint and then delegating to the base class. Moreover, each class forwards the methods it
+does not implement to the next class in the chain.
+
+## Select
+
 Selection is supported by means of structures implementing the
 [`Select`](crate::traits::Select)/[`SelectZero`](crate::traits::SelectZero),
 usually adding an index to structures, such as [`BitVec`](crate::bits::bit_vec::BitVec),
