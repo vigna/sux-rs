fa5515360585f59be0880bf40604dab41267c575
micheleAndreata <michele.andreata@studenti.unimi.it>
2024-02-14 18:39:35 +0100

    Benches for Rank11

--- benches/my_benchmarks/bench_rank.rs
+++ benches/my_benchmarks/bench_rank.rs
@@ -2,7 +2,7 @@ use criterion::{black_box, BenchmarkId, Criterion};
 use rand::rngs::SmallRng;
 use rand::{Rng, SeedableRng};
 use sux::bits::bit_vec::BitVec;
-use sux::rank_sel::Rank9;
+use sux::rank_sel::{Rank11, Rank9};
 use sux::traits::Rank;
 
 pub fn bench_rank9(c: &mut Criterion) {
@@ -20,36 +20,74 @@ pub fn bench_rank9(c: &mut Criterion) {
         1 << 28,
         1 << 29,
         1 << 30,
+        1 << 31,
+        1 << 32,
     ];
 
-    let mut bitvecs = Vec::<BitVec>::new();
-    let mut bitvec_ids = Vec::<(u64, f64, u64)>::new();
     let mut rng = SmallRng::seed_from_u64(0);
     for len in lens {
         for density in [0.25, 0.5, 0.75] {
             // possible repetitions
             for i in 0..5 {
-                let bitvec = (0..len).map(|_| rng.gen_bool(density)).collect::<BitVec>();
-                bitvecs.push(bitvec);
-                bitvec_ids.push((len, density, i));
+                let bits = (0..len).map(|_| rng.gen_bool(density)).collect::<BitVec>();
+                let rank9: Rank9 = Rank9::new(bits);
+                bench_group.bench_function(
+                    BenchmarkId::from_parameter(format!("{}_{}_{}", len, density, i)),
+                    |b| {
+                        b.iter(|| {
+                            // use fastrange
+                            let r = ((rng.gen::<u64>() as u128).wrapping_mul(len as u128) >> 64)
+                                as usize;
+                            black_box(unsafe { rank9.rank_unchecked(r) });
+                        })
+                    },
+                );
             }
         }
     }
 
-    for (bitvec, bitvec_id) in std::iter::zip(&bitvecs, &bitvec_ids) {
-        let bits = bitvec.clone();
-        let len = bits.len();
-        let rank9: Rank9 = Rank9::new(bits);
-        bench_group.bench_function(
-            BenchmarkId::from_parameter(format!("{}_{}_{}", bitvec_id.0, bitvec_id.1, bitvec_id.2)),
-            |b| {
-                b.iter(|| {
-                    // use fastrange
-                    let r = ((rng.gen::<u64>() as u128).wrapping_mul(len as u128) >> 64) as usize;
-                    black_box(unsafe { rank9.rank_unchecked(r) });
-                })
-            },
-        );
+    bench_group.finish();
+}
+
+pub fn bench_rank11(c: &mut Criterion) {
+    let mut bench_group = c.benchmark_group("rank11");
+
+    let lens = [
+        1u64 << 20,
+        1 << 21,
+        1 << 22,
+        1 << 23,
+        1 << 24,
+        1 << 25,
+        1 << 26,
+        1 << 27,
+        1 << 28,
+        1 << 29,
+        1 << 30,
+        1 << 31,
+        1 << 32,
+    ];
+
+    let mut rng = SmallRng::seed_from_u64(0);
+    for len in lens {
+        for density in [0.25, 0.5, 0.75] {
+            // possible repetitions
+            for i in 0..5 {
+                let bits = (0..len).map(|_| rng.gen_bool(density)).collect::<BitVec>();
+                let rank11: Rank11 = Rank11::new(bits);
+                bench_group.bench_function(
+                    BenchmarkId::from_parameter(format!("{}_{}_{}", len, density, i)),
+                    |b| {
+                        b.iter(|| {
+                            // use fastrange
+                            let r = ((rng.gen::<u64>() as u128).wrapping_mul(len as u128) >> 64)
+                                as usize;
+                            black_box(unsafe { rank11.rank_unchecked(r) });
+                        })
+                    },
+                );
+            }
+        }
     }
 
     bench_group.finish();
--- benches/my_benchmarks/main.rs
+++ benches/my_benchmarks/main.rs
@@ -1,8 +1,8 @@
 mod bench_rank;
 mod bench_select;
-use bench_rank::bench_rank9;
+use bench_rank::{bench_rank11, bench_rank9};
 use bench_select::bench_select_fixed2;
 use criterion::{criterion_group, criterion_main};
 
-criterion_group!(benches, bench_rank9, bench_select_fixed2);
+criterion_group!(benches, bench_rank9, bench_rank11, bench_select_fixed2);
 criterion_main!(benches);
