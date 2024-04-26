1b2858acb959e9e656e39c2c6fa5e2e4b3412874
micheleAndreata <michele.andreata@studenti.unimi.it>
2024-04-26 19:12:01 +0200

    fixes on rank10

--- benches/my_benchmarks/bench_rank.rs
+++ benches/my_benchmarks/bench_rank.rs
@@ -2,7 +2,7 @@ use criterion::{black_box, BenchmarkId, Criterion};
 use rand::rngs::SmallRng;
 use rand::{Rng, SeedableRng};
 use sux::bits::bit_vec::BitVec;
-use sux::rank_sel::{Rank11, Rank12, Rank16, Rank9};
+use sux::rank_sel::{Rank10, Rank11, Rank12, Rank16, Rank9};
 use sux::traits::Rank;
 
 const LENS: [u64; 11] = [
@@ -41,6 +41,34 @@ pub fn bench_rank9(c: &mut Criterion) {
                             let p = ((rng.gen::<u64>() as u128).wrapping_mul(len as u128) >> 64)
                                 as usize;
                             black_box(unsafe { rank9.rank_unchecked(p) });
+                        })
+                    },
+                );
+            }
+        }
+    }
+
+    bench_group.finish();
+}
+
+pub fn bench_rank10(c: &mut Criterion) {
+    let mut bench_group = c.benchmark_group("rank10");
+
+    let mut rng = SmallRng::seed_from_u64(0);
+    for len in LENS {
+        for density in DENSITIES {
+            // possible repetitions
+            for i in 0..REPS {
+                let bits = (0..len).map(|_| rng.gen_bool(density)).collect::<BitVec>();
+                let rank10: Rank10 = Rank10::new(bits);
+                bench_group.bench_function(
+                    BenchmarkId::from_parameter(format!("{}_{}_{}", len, density, i)),
+                    |b| {
+                        b.iter(|| {
+                            // use fastrange
+                            let p = ((rng.gen::<u64>() as u128).wrapping_mul(len as u128) >> 64)
+                                as usize;
+                            black_box(unsafe { rank10.rank_unchecked(p) });
                         })
                     },
                 );
