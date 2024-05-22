4231988ca5add5de4c87c0f0453bf9acb287feb8
micheleAndreata <michele.andreata@studenti.unimi.it>
2024-05-22 22:05:57 +0200

    simple and fixed2 comparison

--- benches/my_benchmarks/bench_select.rs
+++ benches/my_benchmarks/bench_select.rs
@@ -8,6 +8,7 @@ use rand::{Rng, SeedableRng};
 use sux::bits::bit_vec::BitVec;
 use sux::rank_sel::Rank10Sel;
 use sux::rank_sel::Rank9Sel;
+use sux::rank_sel::SelectFixed2;
 use sux::rank_sel::SimpleSelect;
 use sux::traits::Select;
 
@@ -154,5 +155,61 @@ pub fn bench_rank10sel<const LOG2_LOWER_BLOCK_SIZE: usize, const LOG2_ONES_PER_I
     bench_select::<sux::rank_sel::Rank10Sel<LOG2_LOWER_BLOCK_SIZE, LOG2_ONES_PER_INVENTORY>>(
         &mut group, &LENS, &DENSITIES, REPS, uniform,
     );
+    group.finish();
+}
+
+macro_rules! bench_select_fixed2 {
+    ([$($inv_size:literal),+], $subinv_size:tt, $bitvecs:ident, $bitvec_ids:ident, $bench_group:expr) => {
+        $(
+            bench_select_fixed2!($inv_size, $subinv_size, $bitvecs, $bitvec_ids, $bench_group);
+        )+
+    };
+    ($inv_size:literal, [$($subinv_size:literal),+], $bitvecs:ident, $bitvec_ids:ident, $bench_group:expr) => {
+        $(
+            bench_select_fixed2!($inv_size, $subinv_size, $bitvecs, $bitvec_ids, $bench_group);
+        )+
+    };
+    ($log_inv_size:literal, $log_subinv_size:literal, $bitvecs:ident, $bitvec_ids:ident, $bench_group:expr) => {{
+        let mut rng = SmallRng::seed_from_u64(0);
+        for (bitvec, bitvec_id) in std::iter::zip(&$bitvecs, &$bitvec_ids) {
+            let bits = bitvec.clone();
+            let num_ones = bits.count_ones();
+            let sel: SelectFixed2<BitVec, Vec<u64>, $log_inv_size, $log_subinv_size> =
+                SelectFixed2::new(bits);
+            $bench_group.bench_with_input(
+                BenchmarkId::from_parameter(format!(
+                    "{}_{}_{}_{}",
+                    $log_inv_size, $log_subinv_size, bitvec_id.0, bitvec_id.1
+                )),
+                &$log_inv_size,
+                |b, _| {
+                    b.iter(|| {
+                        // use fastrange
+                        let r =  ((rng.gen::<u64>() as u128).wrapping_mul(num_ones as u128) >> 64) as usize;
+                        black_box(unsafe { sel.select_unchecked(r) });
+                    })
+                },
+            );
+        }
+    }};
+}
+
+pub fn compare_simple_fixed(c: &mut Criterion) {
+    let lens = [1_000_000, 10_000_000, 100_000_000, 1_000_000_000];
+    let mut group = c.benchmark_group("select_fixed2");
+
+    let mut bitvecs = Vec::<BitVec>::new();
+    let mut bitvec_ids = Vec::<(u64, f64)>::new();
+    let mut rng = SmallRng::seed_from_u64(0);
+    for len in lens {
+        for density in [0.5] {
+            // possible repetitions
+            let bitvec = (0..len).map(|_| rng.gen_bool(density)).collect::<BitVec>();
+            bitvecs.push(bitvec);
+            bitvec_ids.push((len, density));
+        }
+    }
+
+    bench_select_fixed2!([10, 11, 12], [3], bitvecs, bitvec_ids, group);
     group.finish();
 }
