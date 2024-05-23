a867844e94a62856c14d45266eaf41706e13ce99
micheleAndreata <michele.andreata@studenti.unimi.it>
2024-05-23 11:23:03 +0200

    simple & fixed2 comparison

--- benches/plot-benches.py
+++ benches/plot-benches.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
@@ -9,18 +7,15 @@ import math
 import pandas as pd
 import numpy as np
 
-colors = ['b', 'g', 'r', 'c', 'm']
-markers = ["v", "o", "+", "*", "^", "s", "D", "x"]
-num_of_densities = 3
+colors = ['b', 'g', 'r', 'c', 'm', 'purple',
+          'gold', 'teal', 'orange', 'brown', 'pink']
+markers = np.array(["v", "o", "+", "*", "^", "s", "D", "x"])
 
 
 def load_benches(base_path):
     benches_list = []
 
-    for dir in os.listdir(base_path):
-        if not os.path.isdir(base_path + dir):
-            continue
-
+    for dir in sorted([d for d in os.listdir(base_path) if os.path.isdir(base_path + d)]):
         run_name = dir.split("_")
         data = {}
 
@@ -31,96 +26,47 @@ def load_benches(base_path):
         path = base_path + dir + "/new/estimates.json"
         with open(path, "r") as f:
             estimates = json.load(f)
-            data["time"] = estimates["median"]["point_estimate"]
-
+            data["time"] = estimates["mean"]["point_estimate"]
         benches_list.append(data)
 
     benches_df = pd.DataFrame(benches_list)
     benches_df = benches_df.groupby(
-        ["size", "dense"], as_index=False)["time"].median()
+        ["size", "dense"], as_index=False)["time"].mean()
+
     benches_df = benches_df.sort_values(by="size", ignore_index=True)
     return benches_df
 
 
-def compare_benches(benches, compare_name):
-    fig, ax = plt.subplots(1, len(
-        benches), constrained_layout=True, sharex=True, sharey=True, squeeze=False)
+def compare_benches(benches, compare_name, op_type):
+    num_densities = len(benches[0][0]["dense"].unique())
+    fig, ax = plt.subplots(1, num_densities, constrained_layout=True,
+                           sharex=True, sharey=True, squeeze=False)
     fig.set_size_inches(10, 6)
     fig.text(0.5, -0.02, 'size [num of bits]', ha='center', va='center')
-    fig.text(-0.01, 0.5, 'time [ns]', ha='center',
+    fig.text(-0.01, 0.5, f'time [ns/{op_type}]', ha='center',
              va='center', rotation='vertical')
 
     for i, (bench, bench_name) in enumerate(benches):
         for d, (name, group) in enumerate(bench.groupby("dense")):
-            ax[0, i].plot(group["size"], group["time"], label=f"density={float(name)*100}%",
-                          color=colors[d], marker="o", markersize=3, linewidth=1.0)
-        ax[0, i].set_title(bench_name)
-        ax[0, i].grid(True)
-        ax[0, i].set_xscale("log")
-        ax[0, i].set_yscale("log")
+            ax[0, d].plot(group["size"], group["time"], label=bench_name,
+                          color=colors[i], marker=markers[i], markersize=3, linewidth=1.0)
+            ax[0, d].set_title(f"density={float(name)*100}%")
+            ax[0, d].grid(True)
+            ax[0, d].set_xscale("log")
+            ax[0, d].set_yscale("log")
 
     times = np.sort(np.concatenate(
         list(map(lambda x: x[0]["time"].unique(), benches)), axis=0))
-    ticks = np.logspace(np.log10(times[0]), np.log10(times[-1]), num=6)
+    ticks = np.logspace(np.log10(times[0]), np.log10(times[-1]), num=8)
     ticks = list(map(lambda x: math.ceil(x), ticks))
     ax[0, 0].set_yticks(ticks)
     ax[0, 0].set_yticklabels(ticks)
+    ax[0, 0].yaxis.set_minor_locator(plt.NullLocator())
 
-    h1, l1 = ax[0, 0].get_legend_handles_labels()
+    h1, _ = ax[0, 0].get_legend_handles_labels()
     fig.legend(handles=h1, loc='upper center', bbox_to_anchor=(
-        0.5, -0.04), fancybox=True, shadow=True, ncol=5)
-
-    plt.savefig("./plots/{}.svg".format(compare_name),
-                format="svg", bbox_inches="tight")
-    plt.close(fig)
-
-
-def compare_benches_same_plot(bench1, bench2, compare_name):
-    fig, ax = plt.subplots(1, 1, constrained_layout=True)
-    fig.set_size_inches(10, 6)
-    fig.text(0.5, -0.02, 'size [num of bits]', ha='center', va='center')
-    fig.text(-0.01, 0.5, 'time [ns]', ha='center',
-             va='center', rotation='vertical')
-
-    for d, (name, group) in enumerate(bench1[0].groupby("dense")):
-        ax.plot(group["size"], group["time"], label=f"density={float(name)*100}%",
-                color=colors[d], marker=markers[0], markersize=3, linewidth=1.0, linestyle="-")
-
-    for d, (name, group) in enumerate(bench2[0].groupby("dense")):
-        ax.plot(group["size"], group["time"], label=f"density={float(name)*100}%",
-                color=colors[d], marker=markers[1], markersize=3, linewidth=1.0, linestyle="--")
-
-    times = []
-    times.append(bench1[0]["time"].unique())
-    times.append(bench2[0]["time"].unique())
-
-    times = np.unique(np.concatenate(times), axis=0)
-    ticks = np.logspace(np.log10(times[0]), np.log10(times[-1]), num=6)
-    ticks = list(map(lambda x: round(x, 1), ticks))
-
-    ax.set_xscale("log")
-    ax.set_yscale("log")
-    ax.set_yticks(ticks)
-    ax.set_yticklabels(ticks)
-    ax.grid(True)
-    ax.yaxis.set_minor_locator(plt.NullLocator())
-
-    density25_patch = mpatches.Patch(color='blue', label='density=25%')
-    density50_patch = mpatches.Patch(color='green', label='density=50%')
-    density75_patch = mpatches.Patch(color='red', label='density=75%')
-
-    marker1 = Line2D([0], [0], color='black',
-                     linewidth=1.0, linestyle="-", marker=markers[0], markersize=4, label=bench1[1])
-    marker2 = Line2D([0], [0], color='black',
-                     linewidth=1.0, linestyle="--", marker=markers[1], markersize=4, label=bench2[1])
-
-    handles = [density25_patch, density50_patch,
-               density75_patch, marker1, marker2]
-
-    fig.legend(handles=handles, loc='upper center', bbox_to_anchor=(
         0.5, -0.04), fancybox=True, shadow=True, ncol=3)
 
-    plt.draw_all()
     plt.savefig("./plots/{}.svg".format(compare_name),
                 format="svg", bbox_inches="tight")
     plt.close(fig)
@@ -128,9 +74,8 @@ def compare_benches_same_plot(bench1, bench2, compare_name):
 
 if __name__ == "__main__":
     benches = []
-    for file in os.listdir("../target/criterion/"):
-        if os.path.isdir(f"../target/criterion/{file}/"):
+    for file in os.listdir("target/criterion/"):
+        if os.path.isdir(f"target/criterion/{file}/"):
             benches.append(
-                (load_benches(f"../target/criterion/{file}/"), file))
-
-    compare_benches(benches, "benches")
+                (load_benches(f"target/criterion/{file}/"), file))
+    compare_benches(benches, "benches", "select")
--- src/bin/find_simple_params.rs
+++ src/bin/find_simple_params.rs
@@ -4,7 +4,15 @@ use rand::{rngs::SmallRng, Rng, SeedableRng};
 use sux::{bits::BitVec, rank_sel::SimpleSelect};
 
 fn main() {
-    let lens = [1_000_000, 10_000_000, 100_000_000, 1_000_000_000];
+    let lens = [
+        1_000_000,
+        3_000_000,
+        10_000_000,
+        30_000_000,
+        100_000_000,
+        300_000_000,
+        1_000_000_000,
+    ];
     let densities = [0.2, 0.5, 0.8];
 
     let mut bitvecs = Vec::<BitVec>::new();
@@ -18,12 +26,21 @@ fn main() {
         }
     }
 
+    let wd = std::env::current_dir().unwrap().display().to_string();
+    let source_path = format!("{}/target/criterion/select_fixed2/", wd);
+    let target_path = format!("{}/target/criterion/fixed_filtered/", wd);
+
     for (b, (len, d)) in zip(&bitvecs, &bitvec_ids) {
         let simple = SimpleSelect::new(b.clone(), 3);
-        println!("--- bitvec with len: {}, density: {}", len, d);
         let log2_ones_per_inventory = simple.get_log2_ones_per_inventory();
         let log2_u64_per_subinventory = simple.get_log2_u64_per_subinventory();
-        println!("log2_ones_per_inventory: {}", log2_ones_per_inventory);
-        println!("log2_u64_per_subinventory: {}", log2_u64_per_subinventory);
+        let source_folder_name = format!(
+            "{}_{}_{}_{}_0/",
+            log2_ones_per_inventory, log2_u64_per_subinventory, len, d
+        );
+        let target_folder_name = format!("{}_{}_0", len, d);
+        let source = format!("{}{}", source_path, source_folder_name);
+        let target = format!("{}{}", target_path, target_folder_name);
+        std::fs::rename(source, target).unwrap();
     }
 }
