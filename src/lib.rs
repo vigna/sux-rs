c89a127e5229c49854de72016b58540939e3cb12
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2024-06-03 22:22:19 +0200

    Better include

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
  */
 
-#![doc = include_str!("../README.md")]
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
 #![deny(unconditional_recursion)]
 
 #[cfg(not(target_pointer_width = "64"))]
