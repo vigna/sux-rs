85cabe3f4b212c2e400acb6212ff85bb2597be9b
micheleAndreata <michele.andreata@studenti.unimi.it>
2024-06-06 00:07:32 +0200

    tests for simple_select_const

--- tests/test_simple_select_const.rs
+++ tests/test_simple_select_const.rs
@@ -9,22 +9,30 @@ use rand::rngs::SmallRng;
 use rand::Rng;
 use rand::SeedableRng;
 use sux::bits::BitVec;
-use sux::rank_sel::SimpleSelect;
+use sux::bits::CountBitVec;
+use sux::rank_sel::Rank9;
+use sux::rank_sel::SimpleSelectConst;
 use sux::traits::BitCount;
 use sux::traits::BitLength;
 use sux::traits::Select;
 
+const INV: usize = 10;
+const SUB: usize = 2;
+
 #[test]
-fn test_simple_select() {
+fn test_simple_select_const() {
     let lens = (1..100)
         .step_by(10)
         .chain((100_000..1_000_000).step_by(100_000));
     let mut rng = SmallRng::seed_from_u64(0);
     let density = 0.5;
     for len in lens {
-        let bits: BitVec = (0..len).map(|_| rng.gen_bool(density)).collect::<BitVec>();
+        let bits: CountBitVec = (0..len)
+            .map(|_| rng.gen_bool(density))
+            .collect::<BitVec>()
+            .into();
 
-        let simple = SimpleSelect::new(bits.clone(), 3);
+        let simple = SimpleSelectConst::<_, _, INV, SUB>::new(bits.clone());
 
         let ones = simple.count_ones();
         let mut pos = Vec::with_capacity(ones);
@@ -42,14 +50,20 @@ fn test_simple_select() {
 }
 
 #[test]
-fn test_simple_select_mult_usize() {
-    let mut rng = rand::rngs::SmallRng::seed_from_u64(0);
+fn test_simple_select_const_w_rank9() {
+    let lens = (1..100)
+        .step_by(10)
+        .chain((100_000..1_000_000).step_by(100_000));
+    let mut rng = SmallRng::seed_from_u64(0);
     let density = 0.5;
-    for len in (1 << 10..1 << 15).step_by(usize::BITS as _) {
-        let bits = (0..len).map(|_| rng.gen_bool(density)).collect::<BitVec>();
-        let simple = SimpleSelect::new(bits.clone(), 3);
+    for len in lens {
+        let bits: BitVec = (0..len).map(|_| rng.gen_bool(density)).collect::<BitVec>();
 
-        let ones = bits.count_ones();
+        let rank9 = Rank9::new(bits.clone());
+
+        let simple = SimpleSelectConst::<_, _, INV, SUB>::new(rank9);
+
+        let ones = simple.count_ones();
         let mut pos = Vec::with_capacity(ones);
         for i in 0..len {
             if bits[i] {
@@ -65,19 +79,19 @@ fn test_simple_select_mult_usize() {
 }
 
 #[test]
-fn test_simple_select_empty() {
+fn test_simple_select_const_empty() {
     let bits = BitVec::new(0);
-    let simple = SimpleSelect::new(bits.clone(), 3);
+    let simple = SimpleSelectConst::<_, _, INV, SUB>::new(bits.clone());
     assert_eq!(simple.count_ones(), 0);
     assert_eq!(simple.len(), 0);
     assert_eq!(simple.select(0), None);
 }
 
 #[test]
-fn test_simple_select_ones() {
+fn test_simple_select_const_ones() {
     let len = 300_000;
     let bits = (0..len).map(|_| true).collect::<BitVec>();
-    let simple = SimpleSelect::new(bits, 3);
+    let simple = SimpleSelectConst::<_, _, INV, SUB>::new(bits);
     assert_eq!(simple.count_ones(), len);
     assert_eq!(simple.len(), len);
     for i in 0..len {
@@ -86,35 +100,17 @@ fn test_simple_select_ones() {
 }
 
 #[test]
-fn test_simple_select_zeros() {
+fn test_simple_select_const_zeros() {
     let len = 300_000;
     let bits = (0..len).map(|_| false).collect::<BitVec>();
-    let simple = SimpleSelect::new(bits, 3);
+    let simple = SimpleSelectConst::<_, _, INV, SUB>::new(bits);
     assert_eq!(simple.count_ones(), 0);
     assert_eq!(simple.len(), len);
     assert_eq!(simple.select(0), None);
 }
 
 #[test]
-fn test_simple_select_few_ones() {
-    let lens = [1 << 18, 1 << 19, 1 << 20];
-    for len in lens {
-        for num_ones in [1, 2, 4, 8, 16, 32, 64, 128] {
-            let bits = (0..len)
-                .map(|i| i % (len / num_ones) == 0)
-                .collect::<BitVec>();
-            let simple = SimpleSelect::new(bits, 3);
-            assert_eq!(simple.count_ones(), num_ones);
-            assert_eq!(simple.len(), len);
-            for i in 0..num_ones {
-                assert_eq!(simple.select(i), Some(i * (len / num_ones)));
-            }
-        }
-    }
-}
-
-#[test]
-fn test_simple_non_uniform() {
+fn test_simple_select_const_non_uniform() {
     let lens = [1 << 18, 1 << 19, 1 << 20];
 
     let mut rng = SmallRng::seed_from_u64(0);
@@ -150,10 +146,11 @@ fn test_simple_non_uniform() {
             assert!(num_ones_first_half > 0);
             assert!(num_ones_second_half > 0);
 
-            let bits = first_half
+            let bits: CountBitVec = first_half
                 .into_iter()
                 .chain(second_half.into_iter())
-                .collect::<BitVec>();
+                .collect::<BitVec>()
+                .into();
 
             assert_eq!(
                 num_ones_first_half + num_ones_second_half,
@@ -170,7 +167,7 @@ fn test_simple_non_uniform() {
                 }
             }
 
-            let simple = SimpleSelect::new(bits, 3);
+            let simple = SimpleSelectConst::<_, _, INV, SUB>::new(bits);
             for i in 0..(ones) {
                 assert_eq!(simple.select(i), Some(pos[i]));
             }
