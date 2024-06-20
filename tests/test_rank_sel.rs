f976dae2a31f216eb7549e44f96ca3837f5833c5
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2024-06-20 08:34:24 +0200

    Tests for traits

---
+++ tests/test_rank_sel.rs
@@ -0,0 +1,38 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Inria
+ * SPDX-FileCopyrightText: 2023 Sebastiano Vigna
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
+ */
+
+use sux::{
+    bit_vec,
+    rank_sel::Rank9,
+    traits::{AddNumBits, RankZero},
+};
+
+#[test]
+fn test_rank_zero() {
+    let bits = bit_vec![0, 1, 0, 1, 1, 0, 1, 0, 0, 1];
+    let rank9 = Rank9::new(bits);
+    assert_eq!(rank9.rank_zero(0), 0);
+    assert_eq!(rank9.rank_zero(1), 1);
+    assert_eq!(rank9.rank_zero(2), 1);
+    assert_eq!(rank9.rank_zero(3), 2);
+    assert_eq!(rank9.rank_zero(4), 2);
+    assert_eq!(rank9.rank_zero(5), 2);
+    assert_eq!(rank9.rank_zero(6), 3);
+    assert_eq!(rank9.rank_zero(7), 3);
+    assert_eq!(rank9.rank_zero(8), 4);
+    assert_eq!(rank9.rank_zero(9), 5);
+    assert_eq!(rank9.rank_zero(10), 5);
+}
+
+#[test]
+fn test_add_num_bits() {
+    let bits = bit_vec![0, 1, 0, 1, 1, 0, 1, 0, 0, 1];
+    let a: AddNumBits<_> = bits.into();
+    let (b, c) = a.into_raw_parts();
+    // TODO assert_eq!(b, bits);
+    assert_eq!(c, 5);
+}
