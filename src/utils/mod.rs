4a452ee7d0dd3ad871a58925a2b53bae8becd12f
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2024-11-21 17:14:07 +0100

    Correct transmutation for vectors and boxes slices

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -16,12 +16,28 @@ pub use crate::utils::sig_store::*;
 pub mod spooky;
 pub use crate::utils::spooky::*;
 
+/// Transmutes a vector of one type into a vector of another type.
+///
+/// [It is not safe to transmute a
+/// vector](https://doc.rust-lang.org/std/mem/fn.transmute.html). This method
+/// implements a correct transmutation of the vector content.
+///
+/// # Safety
+///
+/// The caller must ensure the source and destination types have the same size
+/// and memory layout.
 pub unsafe fn transmute_vec<S, D>(v: Vec<S>) -> Vec<D> {
     // Ensure the original vector is not dropped
     let mut v = std::mem::ManuallyDrop::new(v);
     Vec::from_raw_parts(v.as_mut_ptr() as *mut D, v.len(), v.capacity())
 }
 
+/// Transmutes a boxed slice of one type into a boxed slice of another type.
+///
+/// # Safety
+///
+/// The caller must ensure the source and destination types have
+/// the same size and memory layout.
 pub unsafe fn transmute_boxed_slice<S, D>(b: Box<[S]>) -> Box<[D]> {
     // Ensure the original boxed value is not dropped
     let mut b = std::mem::ManuallyDrop::new(b);
