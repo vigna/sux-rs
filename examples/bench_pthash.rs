9e183c8fb6310f441779729c46bbe17bcc831e53
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2025-02-21 18:20:41 +0000

    Now we really test pthash + array

--- examples/bench_pthash.rs
+++ examples/bench_pthash.rs
@@ -155,7 +155,7 @@ fn main() -> Result<()> {
 
         pl.start("Querying (independent)...");
         for k in &keys {
-            std::hint::black_box(func.hash(HashableVecu8(k)));
+            std::hint::black_box(output[func.hash(HashableVecu8(k)) as usize]);
         }
         pl.done_with_count(n);
 
@@ -163,7 +163,7 @@ fn main() -> Result<()> {
         pl.start("Querying (dependent)...");
         for k in &mut keys {
             k[0] ^= result as u8;
-            std::hint::black_box(result = func.hash(HashableVecu8(k)));
+            std::hint::black_box(result = output[func.hash(HashableVecu8(k)) as usize]);
         }
         pl.done_with_count(n);
     } else if let Some(n) = args.n {
