7326fa3ca80e7d7252ddb6269dd9aad50fa712ab
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2025-03-06 20:23:54 +0000

    Benches for filters

---
+++ examples/bench_vfilter.rs
@@ -0,0 +1,196 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Sebastiano Vigna
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
+ */
+
+#![allow(clippy::collapsible_else_if)]
+use anyhow::Result;
+use clap::Parser;
+use common_traits::{CastableFrom, DowncastableInto};
+use dsi_progress_logger::*;
+use epserde::prelude::*;
+use lender::*;
+use rdst::RadixKey;
+use sux::{
+    bits::BitFieldVec,
+    func::*,
+    traits::{BitFieldSlice, Word},
+    utils::{LineLender, Sig, SigVal, ToSig, ZstdLineLender},
+};
+
+#[derive(Parser, Debug)]
+#[command(about = "Benchmark VFunc with strings or 64-bit integers", long_about = None)]
+struct Args {
+    /// The maximum number strings to use from the file, or the number of 64-bit keys.
+    n: usize,
+    /// A name for the ε-serde serialized function with u64 keys.
+    func: String,
+    /// The number of bits of the signatures.
+    #[arg(short, long, default_value_t = 8)]
+    bits: u32,
+    #[arg(short = 'f', long)]
+    /// A file containing UTF-8 keys, one per line. If not specified, the 64-bit keys [0..n) are used.
+    filename: Option<String>,
+    /// Whether the file is compressed with zstd.
+    #[arg(short, long)]
+    zstd: bool,
+    /// Use 64-bit signatures.
+    #[arg(long)]
+    sig64: bool,
+    /// Do not use sharding.
+    #[arg(long)]
+    no_shards: bool,
+    /// Use 3-hypergraphs.
+    #[cfg(feature = "mwhc")]
+    #[arg(long, conflicts_with = "sig64")]
+    mwhc: bool,
+}
+
+fn main() -> Result<()> {
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .try_init()?;
+
+    let args = Args::parse();
+
+    #[cfg(feature = "mwhc")]
+    if args.mwhc {
+        return match args.bits {
+            8 => {
+                if args.no_shards {
+                    main_with_types::<u8, [u64; 2], Mwhc3NoShards>(args)
+                } else {
+                    main_with_types::<u8, [u64; 2], Mwhc3Shards>(args)
+                }
+            }
+            16 => {
+                if args.no_shards {
+                    main_with_types::<u16, [u64; 2], Mwhc3NoShards>(args)
+                } else {
+                    main_with_types::<u16, [u64; 2], Mwhc3Shards>(args)
+                }
+            }
+            32 => {
+                if args.no_shards {
+                    main_with_types::<u32, [u64; 2], Mwhc3NoShards>(args)
+                } else {
+                    main_with_types::<u32, [u64; 2], Mwhc3Shards>(args)
+                }
+            }
+            _ => unimplemented!("{}-bit signatures", args.bits),
+        };
+    }
+
+    match args.bits {
+        8 => {
+            if args.no_shards {
+                if args.sig64 {
+                    main_with_types::<u8, [u64; 1], Fuse3NoShards>(args)
+                } else {
+                    main_with_types::<u8, [u64; 2], Fuse3NoShards>(args)
+                }
+            } else {
+                main_with_types::<u8, [u64; 2], Fuse3Shards>(args)
+            }
+        }
+        16 => {
+            if args.no_shards {
+                if args.sig64 {
+                    main_with_types::<u16, [u64; 1], Fuse3NoShards>(args)
+                } else {
+                    main_with_types::<u16, [u64; 2], Fuse3NoShards>(args)
+                }
+            } else {
+                main_with_types::<u16, [u64; 2], Fuse3Shards>(args)
+            }
+        }
+        32 => {
+            if args.no_shards {
+                if args.sig64 {
+                    main_with_types::<u32, [u64; 1], Fuse3NoShards>(args)
+                } else {
+                    main_with_types::<u32, [u64; 2], Fuse3NoShards>(args)
+                }
+            } else {
+                main_with_types::<u32, [u64; 2], Fuse3Shards>(args)
+            }
+        }
+        _ => unimplemented!("{}-bit signatures", args.bits),
+    }
+}
+
+fn main_with_types<W: ZeroCopy + Word, S: Sig + Send + Sync, E: ShardEdge<S, 3>>(
+    args: Args,
+) -> Result<()>
+where
+    W: CastableFrom<u64> + DowncastableInto<u8> + TypeHash + AlignHash,
+    SigVal<S, usize>: RadixKey,
+    SigVal<S, ()>: RadixKey,
+    str: ToSig<S>,
+    String: ToSig<S>,
+    usize: ToSig<S>,
+    Box<[W]>: BitFieldSlice<W>,
+    VFunc<usize, usize, BitFieldVec, S, E>: DeserializeInner + TypeHash + AlignHash,
+    VFunc<str, usize, BitFieldVec, S, E>: DeserializeInner + TypeHash + AlignHash,
+    VFunc<usize, W, Box<[W]>, S, E>: DeserializeInner + TypeHash + AlignHash,
+    VFunc<str, W, Box<[W]>, S, E>: DeserializeInner + TypeHash + AlignHash, // TODO: this is weird
+    VFilter<W, VFunc<usize, W, Box<[W]>, S, E>>: DeserializeInner + TypeHash + AlignHash,
+    VFilter<W, VFunc<str, W, Box<[W]>, S, E>>: DeserializeInner + TypeHash + AlignHash,
+{
+    let mut pl = progress_logger![item_name = "key"];
+
+    if let Some(filename) = args.filename {
+        let mut keys: Vec<_> = if args.zstd {
+            ZstdLineLender::from_path(filename)?
+                .map_into_iter(|x| x.unwrap().to_owned())
+                .take(args.n)
+                .collect()
+        } else {
+            LineLender::from_path(filename)?
+                .map_into_iter(|x| x.unwrap().to_owned())
+                .take(args.n)
+                .collect()
+        };
+
+        let filter = VFilter::<W, VFunc<str, W, Box<[W]>, S, E>>::load_full(&args.func)?;
+
+        pl.start("Querying (independent)...");
+        for key in &keys {
+            std::hint::black_box(filter.get(key));
+        }
+        pl.done_with_count(args.n);
+
+        pl.start("Querying (dependent)...");
+        let mut x = W::ZERO;
+        for key in &mut keys {
+            debug_assert!(!key.is_empty());
+            unsafe {
+                // This as horrible as it can be, and will probably
+                // do harm if a key is the empty string, but we avoid
+                // testing
+                *key.as_bytes_mut().get_unchecked_mut(0) ^= x.downcast() & 1;
+            }
+            x = std::hint::black_box(filter.get(key));
+        }
+        pl.done_with_count(args.n);
+    } else {
+        // No filename
+        let filter = VFilter::<W, VFunc<usize, W, Box<[W]>, S, E>>::load_full(&args.func)?;
+
+        pl.start("Querying (independent)...");
+        for i in 0..args.n {
+            std::hint::black_box(filter.get(&i));
+        }
+        pl.done_with_count(args.n);
+
+        pl.start("Querying (dependent)...");
+        let mut x = 0;
+        for i in 0..args.n {
+            x = filter.contains(&(i ^ (x & 1))) as usize;
+            std::hint::black_box(());
+        }
+        pl.done_with_count(args.n);
+    }
+    Ok(())
+}
