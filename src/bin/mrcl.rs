f2a4d81bfc5c62f70c896960c90f16dd1d957ea9
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2025-11-27 13:37:05 +0530

    Added possibility of inverting the permutation

--- src/bin/mrcl.rs
+++ src/bin/mrcl.rs
@@ -9,7 +9,9 @@ use std::fs;
 use anyhow::Result;
 use clap::Parser;
 use epserde::{deser::Deserialize, ser::Serialize};
+use rayon::iter::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator};
 use sux::{init_env_logger, prelude::*};
+use sync_cell_slice::SyncSlice;
 
 #[derive(Parser, Debug)]
 #[command(about = "Serializes a mapped rear-coded list starting from a rear-coded list and a mapping.", long_about = None)]
@@ -21,8 +23,11 @@ struct Args {
     /// A name for the ε-serde serialized mapped rear-coded list.
     dest: String,
     /// The rear-coded list is not sorted.
-    #[arg(long, default_value_t = false)]
+    #[arg(short, long, default_value_t = false)]
     unsorted: bool,
+    /// Assume the map is a permutation and invert it.
+    #[arg(short, long, default_value_t = false)]
+    invert: bool,
 }
 
 fn main() -> Result<()> {
@@ -32,10 +37,23 @@ fn main() -> Result<()> {
 
     let file = fs::read_to_string(args.map)?;
 
-    let map: Vec<usize> = file
+    let mut map: Vec<usize> = file
         .lines()
         .filter_map(|line| line.trim().parse::<usize>().ok())
         .collect();
+
+    if args.invert {
+        let mut inv_map = vec![0; map.len()];
+        let sync_slice = inv_map.as_sync_slice();
+        map.par_iter()
+            .with_min_len(100000)
+            .enumerate()
+            .for_each(|(i, &x)| {
+                unsafe { sync_slice[x].set(i) };
+            });
+        map = inv_map;
+    }
+
     let map = map.into_boxed_slice();
 
     if args.unsorted {
